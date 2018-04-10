d7312491f0dd986af5a5a0217b184d0ea1f26486
Sam Roberti <samuel.p.roberti@gmail.com>
2018-04-10 20:28:12 +0000

    Changed version requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ChamomileElixir.MixProject do
     [
       app: :chamomile_elixir,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.3.3.3.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
